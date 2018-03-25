2c0e42890dc0f2e3dc32cbeab8a5d2d8aab22b27
Lpmvb <lpmvbs@gmail.com>
2018-03-25 10:28:54 +0800

    init

---
+++ website/languages.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const languages = [
+  {
+    enabled: true,
+    name: '中文',
+    tag: 'zh-CN',
+  },
+  {
+    enabled: false,
+    name: '繁體中文',
+    tag: 'zh-TW',
+  },
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en',
+  },
+];
+module.exports = languages;
